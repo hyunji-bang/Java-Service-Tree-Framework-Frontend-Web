74c301f466e26160fab34ff2b6c5694b6c8c6b41
LeeDongMin <admin@313.co.kr>
2022-06-21 01:48:07 +0900

    [SP-313] #comment Version 등록 페이지 퍼블리싱 #close #time 1h +review SR @dongmin.lee
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- arms/js/pdVersionServiceRegist.js
+++ arms/js/pdVersionServiceRegist.js
@@ -19,6 +19,16 @@ function setSideMenu() {
     },1000);
 }
 
+// --- 에디터 설정 --- //
+CKEDITOR.replace( 'editor' );
+CKEDITOR.replace( 'modal-editor' );
+
+// --- 팝업 띄울때 사이즈 조정 -- //
+$( "#modalPopupId" ).click(function() {
+    var height = $( document ).height() -400;
+    $('.modal-body').find('.cke_contents:eq(0)').css('height',height+'px');
+});
+
 // --- jstree ( product ) 테이블 설정 --- //
 $(function() {
     jsTreeProductBuild();
