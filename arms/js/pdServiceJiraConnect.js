48fc0b1d412427aa52c3fdb6768499cc642edc6d
박지윤 <jyun1937@naver.com>
2022-07-03 22:14:01 +0900

    sidemenu, tableData 함수 commom.js로 이동  및 product 페이지 sidemenu, tableData 함수 정리

--- arms/js/pdServiceJiraConnect.js
+++ arms/js/pdServiceJiraConnect.js
@@ -1,22 +1,6 @@
 $(function () {
-    setSideMenu();
+    setSideMenu("jira_connect");
 });
-
-function setSideMenu() {
-    setTimeout(function(){
-        $('#sidebar_menu_product').attr("class","accordion-toggle active");
-        $('#sidebar_menu_product').attr("aria-expanded","true");
-        $('#sidebar_menu_product').css({'color':'lightblue'});
-        $('#sidebar_menu_product').css({'font-weight':'900'});
-
-        $('#product-elements-collapse').attr("class","panel-collapse collapse in");
-        $('#product-elements-collapse').attr("aria-expanded","true");
-
-        $('#sidebar_menu_product_jira_connect').addClass("active");
-        $('#sidebar_menu_product_jira_connect').css({'color':'lightblue'});
-        $('#sidebar_menu_product_jira_connect').css({'font-weight':'900'});
-    },1000);
-}
 
 $(function () {
     jsTreeBuild();
