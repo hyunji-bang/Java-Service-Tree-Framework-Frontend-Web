e33021be4097bbed332d5845a152c1f0629375b9
su-record <su.lifestory@gmail.com>
2022-07-07 23:29:11 +0900

    [MOD] history 작업중

--- arms/js/reqReviewDetail.js
+++ arms/js/reqReviewDetail.js
@@ -9,6 +9,50 @@ $(function () {
 
 const info = {
 	title: "리뷰 bla bla bla bla bla",
+	history: [
+		{
+			updated: new Date().toISOString(),
+			description: "history summary 1",
+			value: 1,
+		},
+		{
+			updated: new Date().toISOString(),
+			description: "history summary 2",
+			value: 2,
+		},
+		{
+			updated: new Date().toISOString(),
+			description: "history summary 3",
+			value: 3,
+		},
+		{
+			updated: new Date().toISOString(),
+			description: "history summary 4",
+			value: 4,
+		},
+		{
+			updated: new Date().toISOString(),
+			description: "history summary 5",
+			value: 5,
+		},
+	],
 };
 
-function makeHistory() {}
+function makeHistory() {
+	const historys = document.querySelector(".review-history");
+
+	let lists = "";
+
+	info.history.forEach((item) => {
+		lists += `
+			<li class="dd-item" data-id="${item.value}">
+                <div class="dd-handle">${item.description}</div>
+                <time>${item.updated}</time>
+            </li>
+		`;
+	});
+
+	historys.innerHTML = lists;
+}
+
+makeHistory();
