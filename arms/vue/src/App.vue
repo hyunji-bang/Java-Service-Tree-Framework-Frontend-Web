f8b33ad4557783812018e2fae1d543ce72a573b6
함수원 <dev.code.su@gmail.com>
2022-07-16 02:48:41 +0900

    [ADD] bootstrap load

--- arms/vue/src/App.vue
+++ arms/vue/src/App.vue
@@ -3,6 +3,7 @@
 </template>
 
 <style lang="scss">
+@import 'bootstrap/dist/css/bootstrap.min.css';
 @import 'bootstrap-icons/font/bootstrap-icons.css';
 @import './assets/css/application.min.css';
 @import './assets/css/override.css';
