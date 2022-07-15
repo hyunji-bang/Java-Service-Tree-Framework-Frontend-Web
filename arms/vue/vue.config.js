d43af587dd2d4a443763fef36e42dd95c18dc952
함수원 <dev.code.su@gmail.com>
2022-07-15 19:31:54 +0900

    [ADD] file set

--- arms/vue/vue.config.js
+++ arms/vue/vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-	transpileDependencies: true,
+  transpileDependencies: true,
+  assetsDir: 'assets',
+  productionSourceMap: false,
+  chainWebpack: config => {
+    config.resolve.alias.set('@', path.resolve(__dirname, 'src/'));
+    config.module.rule('images').set('parser', {
+      dataUrlCondition: {
+        maxSize: 4 * 1024,
+      },
+    });
+  },
 });
