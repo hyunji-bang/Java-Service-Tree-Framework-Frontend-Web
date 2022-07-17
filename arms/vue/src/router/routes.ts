0b0e7c9c3801c2b312fda14107e7c105cf440302
LeeDongMin <admin@313.co.kr>
2022-07-17 20:35:07 +0900

    [SP-313] #comment 루트의 vue 샘플을 삭제함. ( 필요없는 옵션 ) #close #time 1h +review SR @dongmin.lee
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- arms/vue/src/router/routes.ts
+++ arms/vue/src/router/routes.ts
@@ -14,6 +14,14 @@ const routes: RouteRecordRaw[] = [
     name: 'Home',
     component: routeComponent('HomeView'),
   },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
 ];
 
 export default routes;
