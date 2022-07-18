8b8edbcab59c03dd5d5c9e26cc8913e4f0bb6f7d
함수원 <dev.code.su@gmail.com>
2022-07-19 07:17:54 +0900

    [MOD] 요구사항 리뷰

--- arms/js/reqReview.js
+++ arms/js/reqReview.js
@@ -1,13 +1,13 @@
 // --- 사이드 메뉴 -- //
 $(function () {
-    setSideMenu(
-        "sidebar_menu_requirement",
-        "sidebar_menu_requirement_review",
-        "requirement-elements-collapse"
-    );
+	setSideMenu(
+		"sidebar_menu_requirement",
+		"sidebar_menu_requirement_review",
+		"requirement-elements-collapse"
+	);
 });
 
 // 요구사항 상세 보기 클릭 이벤트
 $("#selectView").click(function () {
-    location.href = "reqReviewDetail.html";
+	location.href = "reqReviewDetail.html";
 });
