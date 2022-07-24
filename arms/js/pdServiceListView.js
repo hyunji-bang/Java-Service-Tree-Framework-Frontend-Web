7395d9c4a3455c9ae7dec821171c55426c0f2c3f
LeeDongMin <admin@313.co.kr>
2022-07-24 17:00:28 +0900

    [SP-313] #comment 제품 서비스 조회 및 상세페이지에서 기획서, 제안서 등을 업데이트 할 수 있도록 기능 추가 완료 #close #time 1h +review SR @dongmin.lee
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- arms/js/pdServiceListView.js
+++ arms/js/pdServiceListView.js
@@ -20,24 +20,61 @@ $(function () {
 	var c_id = getUrlParameter('c_id');
 	console.log(c_id);
 
-	// ajax 처리 후 에디터 바인딩.
-	$.ajax({
-		url: '/auth-user/api/arms/pdservice/searchNode.do?c_id=' + c_id,
-		dataType: 'json',
-		async: false,
-		success: function(data) {
-			result = data;
-			console.log(data);
-		}
-	});
 
 	// --- 에디터 설정 --- //
 	CKEDITOR.replace("editor");
-	CKEDITOR.config.readOnly = true;
+	CKEDITOR.config.readOnly = false;
 	CKEDITOR.on("instanceReady", function (ev) {
 		var height = $(document).height() - 450;
 		$(".cke_contents").css("height", height + "px");
 	});
+
+	// ajax 처리 후 에디터 바인딩.
+	$.ajax({
+		url: '/auth-user/api/arms/pdservice/getNode.do?c_id=' + c_id,
+		dataType: 'json',
+		async: false,
+		success: function(data) {
+			CKEDITOR.instances.editor.setData(data.c_contents);
+			var pdServiceNameplaceHolder = $("#pdServiceTitle").attr("placeholder");
+			$("#pdServiceTitle").attr("placeholder",pdServiceNameplaceHolder + " " + data.c_title);
+		}
+	});
+
+
+	$("#regist").click(function() {
+		CKupdate();
+		var formData = $("#editor").html();
+		console.log(formData);
+		$.ajax({
+			type : "POST",
+			url : "/auth-user/api/arms/pdservice/updateContentsToNode.do",
+			cache : false,
+			data : { c_id: c_id, c_contents : formData },
+			dataType : "json",
+			success : function(res) {
+				if (res == "1") {
+					alert("등록이 정상적으로 되었습니다.");
+					//$(location).attr('href',getContextPath()+'/main.do'); //메인화면으로 이동
+					console.log(res);
+				} else if (res.result == "400") {
+					alert("등록이 실패하였습니다.");
+				}
+			},
+			error : onError
+		});
+	});
+
+	//ajax on error
+	function onError(data, status) {
+		alert("ajax error");
+	}
+
+	//AJAX 로 폼의 데이터를 전송할 때 CKEDITOR로 변환 된 textarea값을 다시 변경해줘야 데이터가 전달된다.
+	function CKupdate(){
+		for ( instance in CKEDITOR.instances )
+			CKEDITOR.instances[instance].updateElement();
+	}
 
 	setSideMenu("sidebar_menu_product", "sidebar_menu_product_list");
 	$("#selectList").on("click",function(){
