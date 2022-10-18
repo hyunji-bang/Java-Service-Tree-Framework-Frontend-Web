6f0400ff0fa7b3a7fa58ab2fd42674d0bf0ebb82
LeeDongMin <admin@313.co.kr>
2022-10-18 23:32:09 +0900

    [SP-313] #comment 타입 스크립트는 공부해야 한다. #close #time 1h +review SR @dongmin.lee
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,13 @@
 import {testMakePerson} from "./utils/makePerson";
+import IPerson from "./person/IPerson";
+import Person, { makePerson } from "./person/Person";
+
+import * as R from 'ramda';
+
+let persons: IPerson[] = R.range(0, 5)
+    .map((n: number) => new Person("이동민", 99));
+
+console.log(persons);
+
 
 testMakePerson();
