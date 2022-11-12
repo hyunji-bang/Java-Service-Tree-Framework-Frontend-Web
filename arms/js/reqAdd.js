c7c33a08a1010cccc85de68482bcf6b34a27cc49
LeeDongMin <admin@313.co.kr>
2022-11-12 12:03:58 +0900

    [SP-313] #comment Requirement 카테고리 :: 등록 관리 메뉴 :: 22.11.11 제품(서비스) 선택 자동완성 구현. #close #time 1h +review SR @dongmin.lee
    [ Frontend-Web::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- arms/js/reqAdd.js
+++ arms/js/reqAdd.js
@@ -4,12 +4,40 @@ $(function () {
 		"sidebar_menu_requirement_regist",
 		"requirement-elements-collapse"
 	);
-	jsTreeBuild("#productTree", "reqAdd/T_ARMS_REQADD_131");
+
+	$(".chzn-select").each(function(){
+		$(this).select2($(this).data());
+	});
+
+	$.ajax({
+		url: "/auth-user/api/arms/pdservice/getPdServiceMonitor.do",
+		type: "GET",
+		contentType: "application/json;charset=UTF-8",
+		dataType : "json",
+		progress: true
+	}).done(function(data) {
+
+		for(var k in data){
+			var obj = data[k];
+			//var jira_name = obj.c_title;
+			selectConnectID = obj.c_id;
+			console.log(selectConnectID);
+			var newOption = new Option(obj.c_title, obj.c_id, false, false);
+			$('#country').append(newOption).trigger('change');
+		}
+	}).fail(function(e) {
+		console.log("fail call");
+	}).always(function() {
+		console.log("always call");
+	});
+
+	$('.multiple-select').multipleSelect();
+
 });
 
 // --- 에디터 설정 --- //
-CKEDITOR.replace("editor");
-CKEDITOR.replace("modal-editor");
+//CKEDITOR.replace("editor");
+//CKEDITOR.replace("modal-editor");
 
 // --- 팝업 띄울때 사이즈 조정 -- //
 $("#modalPopupId").click(function () {
@@ -24,15 +52,13 @@ function jsTreeClick(selectedNodeID) {
 	console.log(selectedNodeID);
 }
 
-// --- select2 (사용자 자동완성 검색 ) 선택하고 나면 선택된 데이터 공간을 벌리기위한 설정 --- //
+// --- select2 ( 제품(서비스) 검색 및 선택 ) --- //
 $('#country').on('select2:selecting', function (e) {
-	console.log("check -> " + $('#country').val());
-	if( $('#country').val() == "1"){
-		jsTreeBuild("#productTree", "reqAdd/T_ARMS_REQADD_131");
-	}else{
-		jsTreeBuild("#productTree", "reqAdd");
-	}
+	// 제품( 서비스 ) 선택했으니까 자동으로 버전을 선택할 수 있게 유도
+	// 디폴트는 base version 을 선택하게 하고 ( select all )
 
+	console.log("check -> " + $('#country').val());
+	jsTreeBuild("#productTree", "reqAdd/T_ARMS_REQADD_" + $('#country').val());
 });
 
 // 신규 제품(서비스) 등록 버튼
